f0dba2928944f800d1cc46e93da84bbb87a06f23
ict <ict@R1-14.local>
2012-09-20 21:51:40 +0900

    p129まで

--- src/sample/application/fingerpaint/FingerPaintActivity.java
+++ src/sample/application/fingerpaint/FingerPaintActivity.java
@@ -1,13 +1,19 @@
 package sample.application.fingerpaint;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.os.Bundle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class FingerPaintActivity extends Activity implements OnTouchListener{
 	
@@ -18,7 +24,60 @@ public class FingerPaintActivity extends Activity implements OnTouchListener{
 	public Float x1,y1;
 	public Integer w,h;
 	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fingerpaint);
+		
+		ImageView iv = (ImageView) this.findViewById(R.id.imageView1);
+		WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+		Display disp = wm.getDefaultDisplay();
+		this.w = disp.getWidth();
+		this.h = disp.getHeight();
+		this.bitmap = Bitmap.createBitmap(this.w, this.h, Bitmap.Config.ARGB_8888);
+		this.paint = new Paint();
+		this.path = new Path();
+		this.canvas = new Canvas(this.bitmap);
+		
+		this.paint.setStrokeWidth(5);
+		this.paint.setStyle(Paint.Style.STROKE);
+		this.paint.setStrokeJoin(Paint.Join.ROUND);
+		this.paint.setStrokeCap(Paint.Cap.ROUND);
+		this.canvas.drawColor(Color.WHITE);
+		iv.setOnTouchListener(this);
+	}
+
+	
 	public boolean onTouch (View v, MotionEvent event){
-		return false;
+		float x = event.getX();
+		float y = event.getY();
+		
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			this.path.reset();
+			this.path.moveTo(x, y);
+			break;
+		case MotionEvent.ACTION_MOVE:
+			this.path.quadTo(x1, y1, x, y);
+			this.x1 = x;
+			this.y1 = y;
+			this.canvas.drawPath(path, paint);
+			this.path.reset();
+			this.path.moveTo(x, y);
+			break;
+		case MotionEvent.ACTION_UP:
+			if (x == this.x1 && y == this.y1 ) {
+				this.y1 = this.y1 + 1;
+			}
+			this.path.quadTo(x1, y1, x, y);
+			this.canvas.drawPath(path, paint);
+			this.path.reset();
+			break;
+		}
+		ImageView iv = (ImageView) this.findViewById(R.id.imageView1);
+		iv.setImageBitmap(this.bitmap);
+		
+		return true;
 	}
 }
